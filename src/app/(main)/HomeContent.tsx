f9c0e2027549aeb13fe20cafb305a293d9f80789
KOSUKE96da <denden.mushi.500ml@gmail.com>
2026-06-28 14:06:35 +0900

    feat: 2-column grid on mobile

--- src/app/(main)/HomeContent.tsx
+++ src/app/(main)/HomeContent.tsx
@@ -92,7 +92,7 @@ export default function HomeContent() {
       </div>
 
       {loading ? (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+        <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3">
           {Array.from({ length: 8 }).map((_, i) => (
             <div
               key={i}
@@ -111,7 +111,7 @@ export default function HomeContent() {
           </p>
         </div>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+        <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3">
           {articles.map((article) => (
             <ArticleCard
               key={article.id}
